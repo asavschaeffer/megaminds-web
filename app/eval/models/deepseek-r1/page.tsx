7361cf13b0c5c67da35632bddbe83d5a5b639c3c
asavschaeffer <avschaeffer@alumni.ubc.ca>
2026-05-17 17:10:53 -0700

    Remove static model page routes

--- app/eval/models/deepseek-r1/page.tsx
+++
@@ -1,6 +0,0 @@
-import { ModelPageTemplate } from '@/components/eval/model-page/model-page-template'
-import { deepseekR1 } from '@/content/eval/models/deepseek-r1'
-
-export default function DeepseekR1Page() {
-  return <ModelPageTemplate profile={deepseekR1} />
-}
--- app/eval/models/template/page.tsx
+++
@@ -1,6 +0,0 @@
-import { ModelPageTemplate } from '@/components/eval/model-page/model-page-template'
-import { templateModel } from '@/content/eval/models/template'
-
-export default function TemplateModelPage() {
-  return <ModelPageTemplate profile={templateModel} />
-}
